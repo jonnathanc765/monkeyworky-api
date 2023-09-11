a40363eb1ca297156ed0c3f9bb7b6be0f434f084
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2023-09-11 08:00:31 -0400

    fix: move seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,13 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
         $this->call(StateSeeder::class);
         $this->call(RolesAndPermissionsSeeder::class);
         $this->call(UserSeeder::class);
-        $this->call(CategoriesSeeder::class);
-        $this->call(DeliveryManagamentSeeder::class);
-        $this->call(BankSeeder::class);
-        $this->call(VariationsSeeder::class);
+
+        if (!app()->environment(['production'])) {
+            $this->call(CategoriesSeeder::class);
+            $this->call(DeliveryManagamentSeeder::class);
+            $this->call(BankSeeder::class);
+            $this->call(VariationsSeeder::class);
+        }
     }
 }
