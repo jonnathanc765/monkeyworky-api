0fc4ce12924772436267a63fa23f9a69f1b85d8c
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2023-09-19 08:18:41 -0400

    fix: cors origin changed¨

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://mirepuesto.com.ve', 'https://api.mirepuesto.com.ve'],
 
     'allowed_origins_patterns' => [],
 
